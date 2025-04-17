dfd45e5c69552fec8ed6316d3632969f064330aa
HelenaBohn <helena_b_martins@estudante.sesisenai.org.br>
2025-04-17 12:49:03 -0300

    add

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ function print(msg) {
     console.log(msg)
 }
 
-
+document.getElementById('formulario').addEventListener('submit', function (e) {
+    e.preventDefault();
+  
+    const campos = [
+      "titulo", "idioma_original", "país_origem", "ano_lançamento", "diretor", "roteiro", "produtora", "elenco", "duração", "genero", "sinopse"];
+  
+    for (let id of campos) {
+      const campo = document.getElementById(id);
+      if (!campo.value.trim()) {
+        alert("O campo \"" + campo.previousElementSibling.textContent + "\" é obrigatório.");
+        campo.focus();
+        return;
+      }
+    }
+  
+    alert("certo!");
+  });
